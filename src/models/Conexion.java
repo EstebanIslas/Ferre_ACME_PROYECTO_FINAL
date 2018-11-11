08be25ade05d3b9de997352752af551c7faf769c
USUARIO <USUARIO@130.100.17.77>
2018-11-11 15:41:33 -0600

    Clase que trae la conexion de base de datos

---
+++ src/models/Conexion.java
@@ -0,0 +1,37 @@
+package models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author EstebanIslas
+ */
+public class Conexion {
+    private final String base = "acme_db";
+    private final String user = "root";
+    private final String password = "";
+    private final String url = "jdbc:mysql://localhost:3306/" + base;
+    private Connection conexion;
+    
+    public Connection getConexion(){
+        try {
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection(this.url, this.user, this.password);
+            
+        } catch (ClassNotFoundException e) {
+            
+            JOptionPane.showMessageDialog(null,"error en clase driver conexion" + e.getMessage());
+            
+        } catch (SQLException ex) {
+            
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conexion;
+    }
+}
